70364d1d650c3501ce29b6332706ed655806c0a8
cramke <carsten.ramke@gmail.com>
2023-04-16 08:53:43 +0200

    Add leaflet map

---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>Map</title>
+	<meta name="description" content="A map to analyze and plan" />
+</svelte:head>
+
+<div>
+	Hey
+</div>
+
+<main>
+    <LeafletMap />
+</main>
+
+<script>
+    import LeafletMap from '$lib/leaflet/leaflet.svelte';
+</script>
